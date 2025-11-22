461c7cdb2617caa2e8c741493af093065c925bf8
IanLucca12 <89036876+IanLucca12@users.noreply.github.com>
2025-11-22 22:43:41 +0000

    Adição de script para gerar admin automaticamente

---
+++ app/create_admin.py
@@ -0,0 +1,34 @@
+
+from app.database import SessionLocal
+from app.models import Usuario, Papel
+from app.main import gerar_hash_senha 
+
+def create_admin():
+    db = SessionLocal()
+    
+    email = "admin@wgg.com"
+    senha = "admin123" 
+
+    # Verifica se já existe
+    user = db.query(Usuario).filter(Usuario.email == email).first()
+    if user:
+        print(f"O usuário {email} já existe")
+        return
+
+    # Cria o Admin
+    print(f"Criando admin: {email}")
+    
+    novo_admin = Usuario(
+        email=email,
+        senha_hashed=gerar_hash_senha(senha),
+        papel=Papel.ADMIN 
+    )
+
+    db.add(novo_admin)
+    db.commit()
+    
+    print("Admin criado")
+    db.close()
+
+if __name__ == "__main__":
+    create_admin()
